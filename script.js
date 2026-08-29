3555d913086470d00b8748431c0d73ff322dafef
begzodbeksodiqov95-cmyk <begzodbeksodiqov95@gmail.com>
2026-08-29 21:50:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,18 @@ let startY = 0;
 let endY = 0;
 
 function showVideo(index) {
-
-    if (index < 0 || index >= videos.length) {
-        return;
-    }
-
-    videos[current].classList.remove("active");
+    if (index < 0 || index >= videos.length) return;
 
     const oldVideo = videos[current].querySelector("video");
+    if (oldVideo) oldVideo.pause();
 
-    if (oldVideo) {
-        oldVideo.pause();
-    }
+    videos[current].classList.remove("active");
 
     current = index;
 
     videos[current].classList.add("active");
 
     const newVideo = videos[current].querySelector("video");
-
     if (newVideo) {
         newVideo.currentTime = 0;
         newVideo.play().catch(() => {});
@@ -35,19 +28,17 @@ document.addEventListener("touchstart", function(e) {
 });
 
 document.addEventListener("touchend", function(e) {
-
     endY = e.changedTouches[0].clientY;
 
     const distance = startY - endY;
 
-    if (Math.abs(distance) < 60) {
-        return;
-    }
+    if (Math.abs(distance) < 60) return;
 
     if (distance > 0) {
+        // Yuqoriga surish
         showVideo(current + 1);
     } else {
+        // Pastga surish
         showVideo(current - 1);
     }
-
 });
